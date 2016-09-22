59300e421f1898c88e33532cf28a4718d45d044e
Tomohiko Mimura <mito.5525@gmail.com>
2016-09-22 23:18:12 +0900

    annotate

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: colleges
+#
+#  id                 :integer          not null, primary key
+#  name               :string(255)      not null
+#  code               :string(1)        not null
+#  default_image_path :string(255)      not null
+#  inserted_at        :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class College < CuentaBase
   has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@
 #  number             :string(9)        not null
 #  encrypted_password :string(255)      not null
 #  college_id         :integer
+#  image_path         :string(255)      not null
 #  inserted_at        :datetime         not null
 #  updated_at         :datetime         not null
 #
