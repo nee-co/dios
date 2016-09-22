010a387edb1d07ade0bb61a64410947ab5528201
Tomohiko Mimura <mito.5525@gmail.com>
2016-09-23 00:40:39 +0900

    ユーザ管理画面を暫定的に整理する

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,15 +1,45 @@
 ActiveAdmin.register User do
+  decorate_with UserDecorator
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  actions :all, except: %i(new create)
+
+  permit_params :number, :name, :college_id
+
+  config.sort_order = 'id_asc'
+
+  index title: 'User' do
+    id_column
+    column '学籍番号', :number
+    column '氏名', :name
+    column 'カレッジ', :college_name
+    column '登録日時', :inserted_at
+    actions
+  end
+
+  filter :id, label: 'ID'
+  filter :number, label: '学籍番号'
+  filter :name, label: '氏名'
+  filter :college, as: :check_boxes, label: 'カレッジ', collection: -> { College.all }
+
+  show do
+    attributes_table do
+      row :id
+      row :number
+      row :name
+      row :college_name
+      row :inserted_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+    inputs 'User' do
+      input :name, required: true
+      input :number, required: true
+      input :college, required: true
+    end
+    actions
+  end
 end
