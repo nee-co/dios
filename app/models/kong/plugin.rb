f6c912a0b7675a421f38cac752cc3961683b28bc
Tomohiko Mimura <mito.5525@gmail.com>
2016-10-20 09:31:13 +0900

    [Kong] CRUD全て(とりあえず)対応
    * 簡単なバリデーション組み込み
    * pluginの追加/削除(jwt, corsのみ)対応
    * 一部デザイン崩れあり(ActiveAdminのDSL分かんね)
    * 不要なAPI呼び出しが多々ありそう => 今後改良予定

--- app/models/kong/plugin.rb
+++ app/models/kong/plugin.rb
@@ -2,6 +2,8 @@ module Kong
   class Plugin < Flexirest::Base
     include Kong::ApiClient
 
+    PERMITTED_PLUGINS = %i(jwt cors).freeze
+
     get :list_per_api, "/apis/:api_id/plugins"
     get :schema, "/plugins/schema/:name"
     delete :delete, "apis/:api_id/plugins/:id"
