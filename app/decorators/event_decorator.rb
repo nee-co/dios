23344df54aea46054c14d6a76c7c30793cbf7409
Tomohiko Mimura <mito.5525@gmail.com>
2016-12-14 23:47:35 +0900

    適当にイベント管理できるようにした

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -1,3 +1,6 @@
 class EventDecorator < Draper::Decorator
   delegate_all
+  decorates_association :comments
+
+  delegate :name, to: :owner, prefix: true, allow_nil: true
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -16,6 +16,6 @@
 class Event < AldeaBase
   has_many :comments, dependent: :delete_all
   has_many :entries, dependent: :delete_all
-  has_many :users, through: :entries
   has_and_belongs_to_many :tags
+  belongs_to :owner, class_name: User
 end
